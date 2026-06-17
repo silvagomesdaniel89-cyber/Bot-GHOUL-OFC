d85c572ca10534ad1c64a6bfca9fb11c41602ace
silvagomesdaniel89-cyber <silvagomesdaniel89@gmail.com>
2026-06-16 23:54:18 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -57,8 +57,9 @@ def home():
     return "O bot está online!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
-
+    # O Render fornece a porta, se não, usa a 8080
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
 def keep_alive():
     t = Thread(target=run)
     t.start()
