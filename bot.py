2f53096bc4204563c54c8da11d267430fe224cb5
silvagomesdaniel89-cyber <silvagomesdaniel89@gmail.com>
2026-07-12 22:25:33 -0300

    bot.py

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ PALAVRAS_PROIBIDAS = [
     "arrombado", "vagabunda", "caralho", "bosta", "merda", "fdp", "fudido", "otario", "idiota", 
     "buceta", "cuzao", "viado", "corno", "puta", "toma no cu", "tmnc", "toma no seu cu", 
     "vai tomar no cu", "se foder", "sfoder", "se fode", "vai se foder", "vai se ferrar", 
-    "vsf", "pqp", "fds", "tomar no cu", "gay", "veado", "LGBT", "ELIGEBETE"
+    "vsf", "pqp", "fds", "tomar no cu", "gay", "veado", "LGBT", "ELIGEBETE", 'eligebete", "Eligebete", "Vai se fuder", "fuder"
 ]
 
 intents = discord.Intents.default()
